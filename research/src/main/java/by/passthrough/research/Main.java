79c042c6394d11cf7c8eea89a64a0f9281d07303
alst0816 <alexandr.stasewich@netcracker.com>
2018-03-13 16:34:37 +0300

    primary filling

---
+++ research/src/main/java/by/passthrough/research/Main.java
@@ -0,0 +1,10 @@
+package by.passthrough.research;
+
+/**
+ * Created by alst0816 on 13.03.2018
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("It works!");
+    }
+}
