58f4b9e8673786bd92a2038f0d6e18605cfe1559
alst0816 <alexandr.stasewich@netcracker.com>
2018-04-07 14:35:44 +0300

    added GroupMessage

---
+++ research/src/main/java/by/passthrough/research/entities/messages/GroupMessage.java
@@ -0,0 +1,11 @@
+package by.passthrough.research.entities.messages;
+
+/**
+ * Created by alst0816 on 07.04.2018
+ */
+public class GroupMessage extends Message {
+    
+    public GroupMessage() {
+        this.messageType = MessageType.GROUP;
+    }
+}
